6408a1ab443ca8b62912f2d9a1b785ad767978bc
tuanlam1610 <73232399+tuanlam1610@users.noreply.github.com>
2022-12-24 15:09:21 +0700

    Complete dynamic website about garage details

--- controllers/garage_info_controller.js
+++ controllers/garage_info_controller.js
@@ -1,33 +1,45 @@
 const models = require('../models/index')
-const { QueryTypes } = require('sequelize');
 
 const controller = {
     show: async (req, res) => {
         res.render('garage_info');
     },
     showDetails: async (req, res) => {
+        let count = 0;
         const garageFound = await models.NhaXe.findOne({
             where: {
                 tennhaxe: req.params.name
             }
         });
         console.log(garageFound.ID_NX);
-        const cmtFound = await models.DanhGia.findAll(
-            {
-                where: {
-                    ID_NX: garageFound.ID_NX
+        var listCMT = []
+        while(true){
+            const cmtFound = await models.DanhGia.findAll(
+                {
+                    where: {
+                        ID_NX: garageFound.ID_NX
+                    },
+                    include: [{
+                        model: models.KhachHang,
+                        attributes: ['hoten'],
+                        require: true
+                    }],
+                    order: [['ngayDanhGia','DESC']],
+                    limit: 2,
+                    offset: count
                 },
-                limit: 16,
-                include: [{
-                    model: models.KhachHang,
-                    attributes: ['hoten'],
-                    require: true
-                }]
-            },
-        )
-        //console.log(cmtFound[0].KhachHang);
+            )
+            count += 2
+            if (cmtFound.length == 0) break
+            else listCMT.push(cmtFound);
+        }
+        const count_CMT = await models.DanhGia.count({
+            where: {
+                ID_NX: garageFound.ID_NX
+            }
+        })
         //res.locals.cmts = cmtFound;
-        res.locals.garage = { garageinfo: garageFound, cmts: cmtFound};
+        res.locals.garage = { garageinfo: garageFound, num_of_cmts: count_CMT, cmts: listCMT};
         res.render('garage_info');
     }
 }
--- controllers/trip_info_controller.js
+++ controllers/trip_info_controller.js
@@ -1,6 +1,13 @@
+const models = require('../models/index');
+const { showDetails } = require('./garage_info_controller');
+
+
 const controller = {
     show: async (req, res) =>{
         res.render('trip_info');
+    },
+    showDetails: async (req, res) => {
+        
     }
 }
 module.exports = controller;
