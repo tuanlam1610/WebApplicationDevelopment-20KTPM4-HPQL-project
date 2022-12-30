02a81fdb430b132c9cc08bf0ff2392a84953d218
NguyenQuang179 <79895324+NguyenQuang179@users.noreply.github.com>
2022-12-30 23:57:07 +0700

    Update List Of Garages Functions
    - Add search function to search garage in list of garages.
    - Add pagination for list of garages.

--- controllers/ListOfGarage_controller.js
+++ controllers/ListOfGarage_controller.js
@@ -1,10 +1,31 @@
 const models = require('../models')
-const { QueryTypes } = require('sequelize');
+const { Op, QueryTypes } = require('sequelize');
 const controller = {
     show: async (req, res) => {
-        res.locals.garages = await models.NhaXe.findAll({
-            order: [["sosaoTB", "DESC"]]
-        });
+        // res.locals.garages = await models.NhaXe.findAll({
+        //     order: [["sosaoTB", "DESC"]]
+        // });
+        var garageName = req.query.garageName;
+        var query = {
+            order: [["sosaoTB", "DESC"]],
+            where: {}
+        }
+        if(req.query.garageName){
+            var regString = "%" + garageName + "%";
+            query.where.tennhaxe = {[Op.iLike]: regString};
+        }
+        // Paginate Setting
+        let page = req.query.page || 1;
+        let limit = 6;
+        query.limit = limit;
+        query.offset = limit * (page - 1);
+        let {rows, count} = await models.NhaXe.findAndCountAll(query);
+        res.locals.garages = rows;
+        res.locals.pagination = {
+            page,
+            limit,
+            totalRows: count,
+        }
         res.render('ListOfGarage', {styleLink: "/assets/css/ListOfGarage.css"});
     }
 }
