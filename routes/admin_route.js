12c4e08763a4e84f371d0e8c400d73af71a87351
tuanlam1610 <73232399+tuanlam1610@users.noreply.github.com>
2022-12-31 22:05:02 +0700

    Delete admin panel

--- routes/admin_route.js
+++ routes/admin_route.js
@@ -7,36 +7,3 @@
 // route.get('/trip/', tripController.show);
 
 // module.exports = route;
-
-const AdminJS = require('adminjs')
-const { ChuyenXe, NhaXe, VeXe, TaiKhoan } = require('../models');
-const adminjs = new AdminJS({
-    rootPath: '/admin',
-    loginPath: '/admin/login',
-    resources: [{
-        resource: ChuyenXe,
-        options: {
-            listProperties: ['IDChuyenXe','ID_NX','tpDi','tpDen','gioKhoiHanh','loaiXe','tongoSoGhe','soGheTrong','giaVe']
-        }
-    }, {
-        resource: NhaXe,
-        options: {
-            listProperties:['ID_NX','tennhaxe','sdt','diachi','email','website','sosaoTB'],
-            properties: {
-                tennhaxe: {
-                    isTitle: true
-                },
-
-                email: {
-                    isTitle: false
-                }
-            }
-        }
-    }
-    ],
-    branding: {
-        companyName: 'HPQL',
-        softwareBrothers: false,
-    }
-});
-module.exports = adminjs;
