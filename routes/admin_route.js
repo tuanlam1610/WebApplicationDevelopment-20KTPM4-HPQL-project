896bef92943fe3ad8ed4b8f08cfe19f431318d0d
tuanlam1610 <73232399+tuanlam1610@users.noreply.github.com>
2022-12-31 21:45:54 +0700

    update admin panel

--- routes/admin_route.js
+++ routes/admin_route.js
@@ -16,19 +16,81 @@ const adminjs = new AdminJS({
     resources: [{
         resource: ChuyenXe,
         options: {
-            listProperties: ['IDChuyenXe','ID_NX','tpDi','tpDen','gioKhoiHanh','loaiXe','tongoSoGhe','soGheTrong','giaVe']
+            listProperties: ['IDChuyenXe', 'ID_NX', 'tpDi', 'tpDen', 'gioKhoiHanh', 'loaiXe', 'tongoSoGhe', 'soGheTrong', 'giaVe'],
+            properties: {
+                ID_NX: {
+                    position: 1,
+                    isRequired: true
+                },
+                tpDi: {
+                    position: 2,
+                    isRequired: true
+                },
+                tpDen: {
+                    position: 3,
+                    isRequired: true
+                },
+                gioKhoiHanh: {
+                    position: 4,
+                    isRequired: true
+                },
+                gioKetThuc: {
+                    position: 5,
+                    isRequired: true
+                },
+                loaiXe: {
+                    position: 6,
+                    availableValues: [{value: "Ghế ngồi", label: "Ghế ngồi"},
+                                      {value: "Giường nằm", label: "Giường nằm"},
+                                      {value: "Giường đôi", label: "Giường đôi"},],
+                    isRequired: true
+                },
+                tongSoGhe: {
+                    position: 7,
+                    isRequired: true
+                },
+                soGheTrong: {
+                    position: 8,
+                    isRequired: true
+                },
+                giaVe: {
+                    position: 9,
+                    isRequired: true
+                },
+                tongThoiGian: {
+                    isVisible: false
+                }
+            }
         }
     }, {
         resource: NhaXe,
         options: {
-            listProperties:['ID_NX','tennhaxe','sdt','diachi','email','website','sosaoTB'],
+            listProperties: ['ID_NX', 'tennhaxe', 'sdt', 'diachi', 'email', 'website', 'sosaoTB'],
             properties: {
                 tennhaxe: {
-                    isTitle: true
+                    isTitle: true,
+                    position: 1,
+                    isRequired: true
                 },
-
                 email: {
-                    isTitle: false
+                    isTitle: false,
+                    position: 2,
+                    isRequired: true
+                },
+                sdt:{
+                    position: 3,
+                    isRequired: true
+                },
+                diachi:{
+                    position: 4,
+                    isRequired: true
+                },
+                website:{
+                    position: 5
+                },
+                sosaoTB:{
+                    position: 6,
+                    isEditable: false
                 }
             }
         }
