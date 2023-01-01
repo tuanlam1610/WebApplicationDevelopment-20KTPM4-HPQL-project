bcb01abff25595a8fed7521d67280fc20431e5e2
tuanlam1610 <73232399+tuanlam1610@users.noreply.github.com>
2023-01-01 17:51:21 +0700

    Add code for user to add a comment about the garage

--- routes/garage_info_route.js
+++ routes/garage_info_route.js
@@ -4,5 +4,6 @@ const controller = require('../controllers/garage_info_controller');
 
 route.get('/', controller.show);
 route.get('/:id', controller.showDetails)
+route.post('/addReview/', controller.addComment)
 
 module.exports = route;
