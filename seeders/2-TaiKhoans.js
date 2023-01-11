45ae7479fc1412faeec53d91655fd7d4c7dc63e4
UnDeadCharger <rexfury121@gmail.com>
2023-01-11 20:36:19 +0700

    Added image, and realize info

--- seeders/2-TaiKhoans.js
+++ seeders/2-TaiKhoans.js
@@ -13,12 +13,12 @@ module.exports = {
      * }], {});
     */
     let items =
-    [{  "email": "tczaple0@people.com.cn", "sdt": "1484297558", "hoten": "Torrey Czaple", "ngaysinh": "2022-01-01", "password": "S5bCnOvhQ", "isAdmin": "false"},
-    {  "email": "ifauguel1@blogs.com", "sdt": "4316013419", "hoten": "Illa Fauguel", "ngaysinh": "2022-01-01", "password": "yTb3LR2ILNB", "isAdmin": "false"},
-    {  "email": "bscarlan2@narod.ru", "sdt": "4013075497", "hoten": "Bone Scarlan", "ngaysinh": "2022-01-01", "password": "UKH2Ag", "isAdmin": "false"},
-    {  "email": "fmarrian3@devhub.com", "sdt": "5958406601", "hoten": "Franni Marrian", "ngaysinh": "2022-01-01", "password": "mk09rE7V", "isAdmin": "false"},
-    {  "email": "kmathan4@rakuten.co.jp", "sdt": "3035927832", "hoten": "Krista Mathan", "ngaysinh": "2022-01-01", "password": "TlSToWWMI", "isAdmin": "false"},
-    {  "email": "tboncoeur5@wufoo.com", "sdt": "5349083640", "hoten": "Terza Boncoeur", "ngaysinh": "2022-01-01", "password": "uLUyEza9", "isAdmin": "false"},
+    [{  "email": "ntphu202@clc.fitus.edu.vn", "sdt": "1484297558", "hoten": "Nguyễn Thiên Phú", "ngaysinh": "2002-06-12", "password": "123", "isAdmin": "false"},
+    {  "email": "nnquang20@clc.fitus.edu.vn", "sdt": "4316013419", "hoten": "Nguyễn Quang", "ngaysinh": "2002-09-17", "password": "123", "isAdmin": "false"},
+    {  "email": "htlam20@clc.fitus.edu.vn", "sdt": "4013075497", "hoten": "Hà Tuân Lâm", "ngaysinh": "2002-10-16", "password": "123", "isAdmin": "false"},
+    {  "email": "tghuy201@clc.fitus.edu.vn", "sdt": "5958406601", "hoten": "Trương Gia Huy", "ngaysinh": "2002-12-06", "password": "123", "isAdmin": "false"},
+    {  "email": "giatruong0612@gmail.com", "sdt": "3035927832", "hoten": "UnDeadCharger", "ngaysinh": "2002-12-06", "password": "123", "isAdmin": "false"},
+    {  "email": "hpqlgroup@gmail.com", "sdt": "5349083640", "hoten": "Admin", "ngaysinh": "2022-01-01", "password": "123", "isAdmin": "true"},
     {  "email": "tparsonage6@1688.com", "sdt": "2082668806", "hoten": "Trev Parsonage", "ngaysinh": "2022-01-01", "password": "kyPqW6xt", "isAdmin": "false"},
     {  "email": "dhaxley7@cargocollective.com", "sdt": "2032606151", "hoten": "Devon Haxley", "ngaysinh": "2022-01-01", "password": "t4eri3X", "isAdmin": "false"},
     {  "email": "rblazejewski8@hc360.com", "sdt": "5162430472", "hoten": "Ruthi Blazejewski", "ngaysinh": "2022-01-01", "password": "tfyXM90CULE", "isAdmin": "false"},
